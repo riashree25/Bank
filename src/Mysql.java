3d21821693ea564241c265934a6d5bf122d6357c
Ria <riashree25@gmail.com>
2017-04-16 23:49:45 +0530

    done

--- src/Mysql.java
+++ src/Mysql.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
-
 /**
  * Created by ria on 16/4/17.
  */
-public class Mysql {
+import java.sql.*;
+class Mysql{
+    public static void main(String args[]){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Bank","root","friends");
+            //here sonoo is database name, root is username and password
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from Personal");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+    }
 }
